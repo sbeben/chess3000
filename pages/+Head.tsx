1e2157865af77c23ed251b01478c2da77a860de1
Senia Merlin <fabriquednb@gmail.com>
2024-12-09 21:24:12 +0400

    font fix

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -6,17 +6,12 @@ export function Head() {
         rel="icon"
         href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 120'%3E%3Cdefs%3E%3CradialGradient id='grad'%3E%3Cstop offset='0%25' stop-color='%239cfc24'/%3E%3Cstop offset='70%25' stop-color='transparent'/%3E%3C/radialGradient%3E%3C/defs%3E%3Ccircle cx='50' cy='60' r='60' fill='url(%23grad)'/%3E%3Ctext y='.9em' font-size='90'%3E♟️%3C/text%3E%3C/svg%3E"
       />
-
-      {/* <link
-        rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>♟️</text></svg>"
-      ></link> */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
-        rel="preload"
         href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Unbounded:wght@200..900&display=swap"
-        as="stylesheet"
+        rel="preload"
+        as="style"
       ></link>
     </>
   );
